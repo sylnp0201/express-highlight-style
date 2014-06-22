8555da3f48c75c6022555f20c6543446fd88d626
Nianpeng Li <linianpeng@gmail.com>
2014-06-21 22:58:18 -0400

    intial commit

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    nodemon: {
+      dev: {
+        script: "server.js"
+      }
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-express');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  // Default task(s).
+  grunt.registerTask('default', ['nodemon']);
+
+};
